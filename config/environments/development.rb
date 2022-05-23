c8b860ffaa67ad72da12d217d0792d4954094cbb
tsainei yuan <hi@tsainei.com>
2022-05-23 21:34:00 +0200

    change default url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => "https://git.heroku.com/all-paw-stuff.git" }
+  config.action_mailer.default_url_options = { :host => "https://git.heroku.com/all-paw-stuff.com" }
 
   config.action_mailer.perform_caching = false
 
