eb6ea0a5fac4b56a46b4209072322656299dada3
kerry pang <kerrypang15@gmail.com>
2022-05-25 14:56:01 +0200

    Attempt to rectify remove function

--- app/policies/accessory_policy.rb
+++ app/policies/accessory_policy.rb
@@ -14,6 +14,12 @@ class AccessoryPolicy < ApplicationPolicy
     record.user == user
   end
 
+  def edit?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
