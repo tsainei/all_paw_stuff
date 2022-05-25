d41469e26d79c0b1606bf525f2d9c435430cb316
tsainei yuan <hi@tsainei.com>
2022-05-25 23:19:36 +0200

    create new action

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,14 +1,18 @@
 class ReservationsController < ApplicationController
-  def index
-  end
+
 
   def show
   end
 
   def new
+    @reservation = Reservation.new
+
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    authorize @reservation
+    @reservation.user = current_user
   end
 
   def destroy
@@ -18,5 +22,18 @@ class ReservationsController < ApplicationController
   end
 
   def edit
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :accessory_id, :start_date, :end_date)
+  end
+
+  def set_accessory
+    @accessory = Accessory.find(params[:reservation_id])
   end
 end
