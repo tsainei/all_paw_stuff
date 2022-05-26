c716a09151b77ac7d9f648cfe4548d3f9fcbd5b2
tsainei yuan <hi@tsainei.com>
2022-05-26 11:35:51 +0200

    i did something with the reservation controller, it's not done yet

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,12 +1,10 @@
 class ReservationsController < ApplicationController
-
-
-  def show
-  end
+  before_action :set_reservation, only: :destroy
+  before_action :set_accessory, only: %i[new create]
+  def show; end
 
   def new
     @reservation = Reservation.new
-
   end
 
   def create
@@ -16,13 +14,13 @@ class ReservationsController < ApplicationController
   end
 
   def destroy
+    @reservation.destroy
+    redirect_to @accessory.index, notice: 'Bookmark was successfully destroyed.'
   end
 
-  def update
-  end
+  def update; end
 
-  def edit
-  end
+  def edit; end
 
   def set_reservation
     @reservation = Reservation.find(params[:id])
@@ -30,10 +28,12 @@ class ReservationsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def reservation_params
-    params.require(:reservation).permit(:user_id, :accessory_id, :start_date, :end_date)
+    params
+      .require(:reservation)
+      .permit(:user_id, :accessory_id, :start_date, :end_date)
   end
 
   def set_accessory
-    @accessory = Accessory.find(params[:reservation_id])
+    @accessory = Accessory.find(params[:accessory_id])
   end
 end
